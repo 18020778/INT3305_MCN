120691f838ed6cab1ff49b63662e21640fbcabf2
18020778 <56502715+18020778@users.noreply.github.com>
2020-09-20 21:08:52 +0700

    week2

--- Week2/binomial.py
+++ Week2/binomial.py
@@ -1,7 +1,7 @@
 import math
 
 def prob(n, p, N):
-    return math.comb(N, n) * (p**N)
+    return math.comb(N, n) * (p ** N)
 
 def infoMeasure(n, p, N):
     return -math.log2(prob(n, p, N))
@@ -12,10 +12,10 @@ def sumProb(N, p):
         sum += prob(i, p, N)
     return sum
 
-    """
+"""
     Dãy sum = prob(1,p,N) + prob(2,p,N) +...+ prob(N,p,N) = (p + 1 - p)**N = 1
     Như vậy, hàm sumProb có thể kiểm chứng được tổng xác suất của phân bố binomial bằng 1
-    """
+"""
 
 def approxEntropy(N, p):
     entropy = 0.0
@@ -23,9 +23,9 @@ def approxEntropy(N, p):
         entropy += prob(i, p, N) * infoMeasure(i, p, N)
     return entropy
 
-    """
+"""
     Các symbol của biến ngẫu nhiên nhị thức là hữu hạn, do đó entropy cũng hữu hạn (bị chặn trên)
     Khi N càng lớn thì approxEntropy(N, p) càng xấp xỉ với entropy nên hàm này cho phép tính xấp xỉ entropy.
-    """
+"""
 
 print(approxEntropy(100, 0.5))
--- Week2/geometric.py
+++ Week2/geometric.py
@@ -11,26 +11,26 @@ def sumProb(N, p):
     for i in range(1, N + 1):
         sum += prob(i,p)
     return sum
-
-    """
+"""
     Dãy sum = prob(1,p) + prob(2,p) +...+ prob(N,p) = 1 - (1-p)**N
     Nếu N càng lớn thì sum càng xấp xỉ 1. 
     Như vậy, hàm sumProb có thể kiểm chứng được tổng xác suất của phân bố geometric bằng 1
-    """
+"""
 
 def approxEntropy(N, p):
+    
     entropy = 0.0
     for i in range(1, N + 1):
-        entropy += prob(i,p) * infoMeasure(n,p)
+        entropy += prob(i,p) * infoMeasure(i,p)
     return entropy 
 
-    """
+print (approxEntropy(1000, 0.5)) # xấp xỉ 2  
+
+"""
     Dãy entropy = prob(1, p) * inforMeasure(1,p) +...+ prob(N, p) * inforMeasure(N, p)
                 = log(1/p) * (1-(1-p)**N) + (1-p)/p * log(1/(1-p))
     Khi N tiến tới vô cùng, dãy entropy = log(1/p) + (1-p)/p * log(1/(1-p)) 
     Như vậy khi dãy entropy có giới hạn như trên thì nó xấp xỉ entropy của nguồn tin geometric
     hay approxEntropy xấp xỉ entropy
-    """
-
-print (approxEntropy(1000, 0.5)) # xấp xỉ 2  
+"""
  
